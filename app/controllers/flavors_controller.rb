d5d77e143f02c788383b7973019af034c012fe79
tiptoemosquito <clhoward33@icloud.com>
2020-12-01 15:35:11 -0500

    app/controllers/flavors_controller

--- app/controllers/flavors_controller.rb
+++ app/controllers/flavors_controller.rb
@@ -1,2 +1,36 @@
 class FlavorsController < ApplicationController
+
+    def index
+        @flavors = Flavor.all
+    end
+
+    def new
+        @flavor = Flavor.new
+    end
+
+    def create
+        @flavor = current_user.flavors.build(flavor_params)
+        if @flavor.save
+            redirect_to flavor_path(@flavor)
+        else
+            redirect_to new_flavor_path
+        end
+    end
+
+    def show
+    end
+
+    def edit
+        @flavor = Flavor.find_by(id: params[:id])
+    end
+
+    def update
+    end
+
+    private
+
+    #strong params
+    def flavor_params
+        params.permit(:name, :ingredients, :alcohiolic, :user_id, :kombucha_id)
+    end
 end
