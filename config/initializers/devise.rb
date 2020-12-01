c17b1993e7245ad30555da72f189683e1cfd2205
tiptoemosquito <clhoward33@icloud.com>
2020-12-01 11:24:50 -0500

    added OAuth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+   config.omniauth :github, '397be65d792d66cc38e9', '90a9a2a0ff72904222ce727469d59de3f542f347', scope: 'user:email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
