a4978d9429c0d22af4f22ee13265a1df60bf97da
tiptoemosquito <clhoward33@icloud.com>
2020-12-02 10:33:14 -0500

    app/models

--- app/models/flavor.rb
+++ app/models/flavor.rb
@@ -1,9 +1,7 @@
 class Flavor < ApplicationRecord
 
-    has_many :kombuchas
+    has_many :kombuchas, dependent: :destroy
     has_many :users, through: :kombuchas
 
     validates :name, presence: true
-    validates :ingredients, presence: true
-    validates :alcoholic, presence: true
 end
