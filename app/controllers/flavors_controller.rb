f624b6d646270ac5f76aedb6922216eb9d218f45
tiptoemosquito <clhoward33@icloud.com>
2020-12-04 17:50:51 -0500

    added validations to controllers

--- app/controllers/flavors_controller.rb
+++ app/controllers/flavors_controller.rb
@@ -2,34 +2,46 @@ class FlavorsController < ApplicationController
     before_action :authenticate_user!
 
     def index
+
         @flavors = current_user.flavors.uniq
+
     end
 
     def new
+
         @flavor = Flavor.new
+
     end
 
     def create
+
         @flavor = current_user.flavors.build(flavor_params)
+
         if @flavor.save
             redirect_to flavor_path(@flavor)
         else
             redirect_to new_flavor_path
         end
+
     end
 
     def show
+
         @flavor = current_user.flavors.find_by(id: params[:id])
+
         if @flavor
             @kombuchas = current_user.kombuchas.where(flavor_id: params[:id])
             render :show
         end
+
     end
 
     private
 
     def flavor_params
+
         params.require(:flavor).permit(:name, :user_id, :kombucha_id)
+        
     end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,29 @@
 class SessionsController < ApplicationController
 
     def new 
+
         @user = User.new
+
     end
 
     def create
+
         @user = User.find_by(email: params[:user][:email])
+
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id 
             redirect_to user_path(@user), notice: "Welcome!"
         else 
             redirect_to new_user_session_path
         end
+
     end
 
     def destroy 
+
         session[:user_id] = nil
         redirect_to root_url
+        
     end
 
 end
